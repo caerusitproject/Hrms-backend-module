64e1da7080788173a92dfdfdbc3c4ff41e3c417a
Shubhradeep Maity <CITC_u0009@caerusitconsulting.com>
2025-10-08 13:27:27 +0530

    pull before

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const sequelize = require('./db');
 const employeeRoutes = require("./routes/employeeRoutes");
@@ -14,6 +15,7 @@ const app = express();
 
 // Swagger
 setupSwagger(app);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use('/api', routes);
