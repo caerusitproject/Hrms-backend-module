f23c870dc9d8a87b00596788367a17c2848c6163
Kaushik Nandy <CITC_u0005@caerusitconsulting.com>
2025-10-13 13:38:41 +0530

    update typo error

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,5 +1,5 @@
 
-const authController = require('../controllers/authController')
+const authController = require('../controllers/authcontroller')
 const express = require("express");
 const router = express.Router();
 
