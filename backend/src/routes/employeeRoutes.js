7becf64f67a2b18a4f43f996522297e00d543904
Shubhradeep Maity <CITC_u0009@caerusitconsulting.com>
2025-10-16 15:40:12 +0530

    Fixes

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -9,13 +9,13 @@ router.post("/login", ctrl.loginEmployee);
 router.post("/create", authenticate, authorizeRoles("HR", "MANAGER","ADMIN"), ctrl.createEmployee);
 router.get("/all", authenticate, authorizeRoles("HR", "MANAGER", "ADMIN"), ctrl.getAllEmployees);
 
-router.get('/managers', authenticate, authorizeRoles("HR", "ADMIN"), ctrl.getAllManagers);
+router.get('/managers', authenticate, authorizeRoles("HR","USER","MANAGER", "ADMIN"), ctrl.getAllManagers);
 // Get manager by ID
-router.get('/manager/:id', authenticate, authorizeRoles("HR", "ADMIN"), ctrl.getManagersById);
-router.get("/managers/subordinate/:managerId", authenticate, authorizeRoles("HR", "ADMIN"), ctrl.getSubordinates);
+router.get('/manager/:id', authenticate, authorizeRoles("HR", "ADMIN", "MANAGER"), ctrl.getManagersById);
+router.get("/managers/subordinate/:managerId", authenticate, authorizeRoles("HR", "MANAGER", "ADMIN"), ctrl.getSubordinates);
 router.patch("/assign/manager", authenticate, authorizeRoles("ADMIN","HR"), ctrl.assignManager);
 
-router.get("/:id", authenticate, authorizeRoles("HR","USER"), ctrl.getEmployeeById);
+router.get("/:id", authenticate, authorizeRoles("HR","USER","MANAGER", "ADMIN"), ctrl.getEmployeeById);
 router.put("/edit/:id", authenticate, authorizeRoles("HR","USER"), ctrl.updateEmployee);
 
 router.get('/managers/subordinate',authenticate, authorizeRoles("HR","ADMIN"),ctrl.getAllManagersWithEmployees);
