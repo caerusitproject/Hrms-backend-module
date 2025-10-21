7db27c0342d965ad2f5bc82aefa125ff4264a1b4
t2471241-arch <tanmay@caerusitconsulting.com>
2025-10-21 16:31:18 +0530

    added get_all_templates

--- backend/src/routes/emailTemplateRoutes.js
+++ backend/src/routes/emailTemplateRoutes.js
@@ -6,20 +6,10 @@ const router = express.Router();
 
 // All routes restricted to authenticated users
 //router.use(authenticate);   
-
-// POST - Create new email template
 router.post('/create',authenticate, authorizeRoles("ADMIN"), emailTemplateController.createTemplate);
-// Patch - Update a template by ID or type
 router.patch('/update',authenticate, authorizeRoles("ADMIN"), emailTemplateController.updateTemplate);
-
-// DELETE - Delete template
 router.delete('/delete',authenticate, authorizeRoles("ADMIN"), emailTemplateController.deleteTemplate);
-
-// GET - Get template by type
-router.get('/type',authenticate, authorizeRoles("ADMIN","HR"), emailTemplateController.getTemplateByType);
-
-//Get the list of all email templates
-router.get('/emails', authenticate, authorizeRoles("ADMIN","HR"), emailTemplateController.getAllTemplates); 
-
-
+router.get('/',authenticate, authorizeRoles("ADMIN","HR"), emailTemplateController.getTemplateByType);
+router.get('/types', authenticate, authorizeRoles("ADMIN","HR"), emailTemplateController.getAllTemplateTypes); 
+router.get('/all', authenticate, authorizeRoles("ADMIN","HR"), emailTemplateController.getAllTemplate);
 module.exports = router;
