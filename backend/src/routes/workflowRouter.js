932c757d66aa87dba81f9631e3f077aeb506bded
Kaushik Nandy <CITC_u0005@caerusitconsulting.com>
2025-11-14 16:27:04 +0530

    update

--- backend/src/routes/workflowRouter.js
+++ backend/src/routes/workflowRouter.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const workflowController = require('../controllers/workflow/workflowController');
+const leaveWorkflow = require('../controllers/workflow/leaveWorkflowController')
 
 router.use('/workflow', workflowController);
+router.use('/leave', leaveWorkflow.create);
+
+/*router.use('/leave', leaveController.create);
+router.use('/leave/:id/submit', leaveController.submit);
+router.use('/leave/:id/manager-approve', leaveController.managerApprove);
+router.use('/leave/:id/hr-approve', leaveController.hrApprove);
+router.use('/leave/:id/finalize', leaveController.finalize);
+router.use('/leave/:id/reject', leaveController.reject);
+router.use('/leave/:id/cancel', leaveController.cancel);*/
 
 module.exports = router;
